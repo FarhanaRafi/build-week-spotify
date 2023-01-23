53728d6275742ca89f35b7209f83c273e97db546
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-01-23 17:09:47 +0100

    minor changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const options = {
     "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
   },
 };
-let songs = [];
+let globalTracks = [];
 
 const getTrackDetails = async (searchQuery) => {
   try {
@@ -21,26 +21,59 @@ const getTrackDetails = async (searchQuery) => {
   }
 };
 
-const renderCards = (tracks, section) => {
+const renderCards = (tracks, section, playable) => {
   let container = document.getElementById(section);
-  let trackCards = tracks.map((track) => {
-    return `<div class="col mb-4">
-    <div class="card h-100" id="${track.album.id}" onclick = "onCardClick(event)">
-      <img src="${track.album.cover_medium}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${track.album.title}</h5>
-        <p class="card-text">${track.artist.name}</p>
-      </div>
-    </div>
-  </div>`;
-  });
+  let trackCards;
+  if (!playable) {
+    trackCards = tracks.map((track) => {
+      globalTracks.push(track);
+      return `<div class="col mb-4">
+        <div class="card h-100" id="${track.album.id}" onclick = "onCardClick(event)">
+          <img src="${track.album.cover_medium}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${track.album.title}</h5>
+            <p class="card-text">${track.artist.name}</p>
+          </div>
+        </div>
+      </div>`;
+    });
+  } else {
+    trackCards = tracks.map((track) => {
+      globalTracks.push(track);
+      return `<div class="col mb-4">
+        <div class="card h-100" id="${track.album.id}">
+        <div class="play-btn-container">
+          <img src="${track.album.cover_medium}" class="card-img-top" alt="...">
+          <span class="play-btn text-success" onclick ="playTrack(${track.id})"><i class="bi bi-play-circle-fill"></i></span>
+        </div>
+          <div class="card-body">
+            <h5 class="card-title">${track.album.title}</h5>
+            <p class="card-text">${track.artist.name}</p>
+          </div>
+        </div>
+      </div>`;
+    });
+  }
 
   container.innerHTML = trackCards.join("");
 };
+const playTrack = (trackId) => {
+  let image = document.getElementById("album-art");
+  let title = document.getElementById("album-title");
+  let artistName = document.getElementById("album-artist");
+  let time = document.getElementById("time-over");
+  let duration = document.getElementById("time-remaining");
+  let selectedTrack = globalTracks.find((track) => track.id === trackId);
+  image.src = selectedTrack.album.cover_small;
+  title.innerText = selectedTrack.album.title;
+  artistName.innerText = selectedTrack.artist.name;
+  duration.innerText = selectedTrack.duration;
 
-const getSection = async (searchQuery, section) => {
+  console.log(selectedTrack);
+};
+const getSection = async (searchQuery, section, playable) => {
   let tracks = await getTrackDetails(searchQuery);
-  renderCards(tracks.slice(0, 5), section);
+  renderCards(tracks.slice(0, 5), section, playable);
 };
 
 const onCardClick = (event) => {
@@ -50,9 +83,9 @@ const onCardClick = (event) => {
 };
 
 const loadSections = () => {
-  getSection("pop", "recent-played");
-  getSection("podcasts", "show-to-try");
-  getSection("mix", "spotify");
+  getSection("pop", "recent-played", false);
+  getSection("podcasts", "show-to-try", false);
+  getSection("mix", "spotify", true);
 };
 
 window.onload = loadSections();
